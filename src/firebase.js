b1c832f26e5170aa0eddc398d6c29d2ede5319c8
jelly0658 <lksolitude@naver.com>
2021-01-13 21:24:15 +0900

    setting finish

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,8 @@
 import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/firestore";
+import "firebase/storage";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -9,4 +13,12 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_APP_ID
 };
 
-export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+export const firebaseInstance = firebase;
+
+export const authService = firebase.auth();
+
+export const dbService = firebase.firestore();
+
+export const storageService = firebase.storage();
