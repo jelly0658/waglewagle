6f737028712659bd01e4de259f8ec55050d07f25
jelly0658 <lksolitude@naver.com>
2021-01-21 00:13:21 +0900

    authtext

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -10,7 +10,23 @@ const Auth = () => {
                 alt="Logo"
                 style={{ width: "200px", height: "auto" }}
             />
+            <span className="authText">
+                <h1 className="authInfo">
+                    이메일과 비밀번호를 입력 후
+                </h1>
+                <h2 className="authInfo">
+                    <red>가입하기</red> 버튼만 눌러도 <strong>바로 가입!</strong>
+                </h2>
+            </span>
             <AuthForm />
+            <span className="authText">
+                <h1 className="authInfo">
+                    개인 정보보호를 위해
+                </h1>
+                <h2 className="authInfo">
+                    <red>"와글와글"</red>은 <strong>어떠한 개인정보도 수집하지 않습니다.</strong>
+                </h2>
+            </span>
         </div>
     );
 };
